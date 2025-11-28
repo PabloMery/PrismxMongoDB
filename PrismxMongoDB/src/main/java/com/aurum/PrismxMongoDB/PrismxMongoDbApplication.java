a0b875a3d67067e704934410974ebb3a054a32bb
PabloMery~ <Pablomery2002@gmail.com>
2025-11-28 20:15:39 -0300

    first commit

---
+++ PrismxMongoDB/src/main/java/com/aurum/PrismxMongoDB/PrismxMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.aurum.PrismxMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrismxMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrismxMongoDbApplication.class, args);
+	}
+
+}
---
+++ PrismxMongoDB/src/test/java/com/aurum/PrismxMongoDB/PrismxMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurum.PrismxMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismxMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
